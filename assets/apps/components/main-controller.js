4470b4a8d9c0dc9fb8340229f397ebc0de7c353e
Accidental82 <Jason.sullenger@gmail.com>
2016-02-03 15:52:13 -0700

    day 1 wrap up

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -1,6 +1,6 @@
 
 app.controller('MainController', function ($scope) {
-   var mc = this;
+    var mc = this;
     $scope.ourStack = [];
     $scope.discFunc = function () {
         if ($scope.newQ) {
@@ -12,30 +12,17 @@ app.controller('MainController', function ($scope) {
             $scope.newQ = "";
         };
     };
-
-    // this.disc = {
-    //     question: "What is love",
-    //     tags: "noFilter",
-    //     date: Date.now(),
-    //     likes: 0,
-    //     dislikes: 0
-    // }
     
     $scope.resp = []
     $scope.respFunc = function (response) {
-        debugger;
         if (response) {
             response.date = Date.now();
             response.likes = 0;
             response.dislikes = 0;
             $scope.resp.push(response);
-            debugger;
             mc.response = "";
         }
     }
-
-
-
 
     $scope.destroy = function (index) {
         $scope.resp.splice(index, 1);
