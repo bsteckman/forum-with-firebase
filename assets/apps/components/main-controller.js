31d2a6cf24d8e3419a7e81bbbbe73105d87e726e
Bert <Bert>
2016-02-03 13:36:35 -0700

    array created

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -1,4 +1,12 @@
-app.controller('MainController', function(){
-    this.test = 'hello';
+/* global $s */
+app.controller('MainController', function($scope){
+$scope.ourStack =[];
+$scope.discFunc = function(){
+    if($scope.newQ){
+        $scope.ourStack.push($scope.newQ);
+    $scope.newQ ="";
+    };
+};
+
     
-})
+});
