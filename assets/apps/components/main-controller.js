e71a2952eb09d70630c6037d006b856685ad0ebf
Bert <Bert>
2016-02-03 13:50:42 -0700

    full object

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -3,27 +3,26 @@ app.controller('MainController', function($scope){
 $scope.ourStack =[];
 $scope.discFunc = function(){
     if($scope.newQ){
+        $scope.newQ.date = Date.now();
+        $scope.newQ.ans =[];
         $scope.ourStack.push($scope.newQ);
     $scope.newQ ="";
     };
 };
 
-    
-<<<<<<< HEAD
-});
-=======
-    this.disc = {
-        question: "What is love",
-        tags: "noFilter",
-        date: Date.now(),
-        ans: [{
-            resp: false,
-            com: "are you high?",
-            up: 0,
-            down: 0,
-            date: Date.now(),
-        }]
-    }
+
+    // this.disc = {
+    //     question: "What is love",
+    //     tags: "noFilter",
+    //     date: Date.now(),
+    //     ans: [{
+    //         resp: false,
+    //         com: "are you high?",
+    //         up: 0,
+    //         down: 0,
+    //         date: Date.now(),
+    //     }]
+    // }
     
 })
->>>>>>> b8f00f90384ef2a5c5e565435203bbfd03ece561
+
