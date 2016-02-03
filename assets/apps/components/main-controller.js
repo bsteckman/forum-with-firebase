d51e04b49f0fd013c5a326b4ce6453042f0f25be
Accidental82 <Jason.sullenger@gmail.com>
2016-02-03 13:12:18 -0700

    adds obj

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -1,4 +1,17 @@
 app.controller('MainController', function(){
     this.test = 'hello';
     
+    var disc = {
+        question: "string",
+        tags: "string",
+        date: date,
+        ans: [{
+            resp: bool,
+            com: "string";
+            up: 0,
+            down: 0,
+            date: date.now(),
+        }]
+    }
+    
 })
