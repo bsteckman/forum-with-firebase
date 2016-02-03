3057b47797206e21fce987aa7ca91e8b5fae82f4
Bert <Bert>
2016-02-03 14:14:08 -0700

    merged

--- assets/apps/components/main-controller.js
+++ assets/apps/components/main-controller.js
@@ -1,6 +1,4 @@
 
-/* global $s */
-<<<<<<< HEAD
 app.controller('MainController', function ($scope) {
     $scope.ourStack = [];
     $scope.discFunc = function () {
@@ -14,38 +12,10 @@ app.controller('MainController', function ($scope) {
         };
     };
 
-
-=======
-app.controller('MainController', function($scope){
-$scope.ourStack =[];
-$scope.discFunc = function(){
-    if($scope.newQ){
-        $scope.newQ.date = Date.now();
-        $scope.newQ.ans =[];
-        $scope.ourStack.push($scope.newQ);
-        $scope.newQ ="";
-    };
-};
->>>>>>> bb593db0c45b0008c34690d0d1246b1bf7dd39e1
-
-
     this.disc = {
         question: "What is love",
         tags: "noFilter",
         date: Date.now(),
-<<<<<<< HEAD
-        ans: [{
-            resp: false,
-            com: "are you high?",
-            up: 0,
-            down: 0,
-            date: Date.now(),
-        }]
-    }
-    $scope.testy = [];
-
-    $scope.addNew = function (response) {
-=======
         likes: 0,
         dislikes: 0
     }
@@ -61,7 +31,6 @@ $scope.discFunc = function(){
     $scope.testy= [];
     
     $scope.addNew = function(response){
->>>>>>> bb593db0c45b0008c34690d0d1246b1bf7dd39e1
         $scope.testy.push(response)
     }
 
@@ -70,15 +39,3 @@ $scope.discFunc = function(){
     }
 
 });
-
-<<<<<<< HEAD
-=======
-// Each item needs a like and dislike property-->
-
-//   $scope.plusOne = function(index) {
-  	// $scope.products[index].likes += 1;
-//   }
-//   $scope.minusOne = function(index) {
-  	// $scope.products[index].dislikes += 1;
-
->>>>>>> bb593db0c45b0008c34690d0d1246b1bf7dd39e1
